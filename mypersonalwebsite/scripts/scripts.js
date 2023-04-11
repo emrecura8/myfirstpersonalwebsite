29831060754daa7a48f14c7f5223869b6f7d50ea
emrecura8 <70863343+emrecura8@users.noreply.github.com>
2023-04-11 17:06:26 +0300

    Add files via upload

---
+++ mypersonalwebsite/scripts/scripts.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $("#calculate").click(function () {
+        var firstpercentage = 0.3;
+        var secondpercentage = 0.4;
+        var thirdpercentage = 0.3;
+
+        total = ctis151 + ctis255 + ctis261;
+
+        myForm.panel.value = "Your average grade is " + total;
+        
+    });
+});
